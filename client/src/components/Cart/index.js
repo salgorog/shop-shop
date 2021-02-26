d85431daa20302ec6d97fb6aede065550ff10a93
Aztec <salgorog@hotmail.com>
2021-02-25 22:27:33 -0700

    implemented shopping cart storage

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+    };
+
+    if (!state.cart.length) {
+      getCart();
+    }
+  }, [state.cart.length, dispatch]);
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
@@ -29,37 +41,37 @@ const Cart = () => {
       </div>
     );
   }
-  
+
   return (
     <div className="cart">
-  <div className="close" onClick={toggleCart}>[close]</div>
-  <h2>Shopping Cart</h2>
-  {state.cart.length ? (
-    <div>
-      {state.cart.map(item => (
-        <CartItem key={item._id} item={item} />
-      ))}
-      <div className="flex-row space-between">
-        <strong>Total: ${calculateTotal()}</strong>
-        {
-          Auth.loggedIn() ?
-            <button>
-              Checkout
+      <div className="close" onClick={toggleCart}>[close]</div>
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map(item => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {
+              Auth.loggedIn() ?
+                <button>
+                  Checkout
             </button>
-            :
-            <span>(log in to check out)</span>
-        }
-      </div>
-    </div>
-  ) : (
-    <h3>
-      <span role="img" aria-label="shocked">
-        😱
+                :
+                <span>(log in to check out)</span>
+            }
+          </div>
+        </div>
+      ) : (
+          <h3>
+            <span role="img" aria-label="shocked">
+              😱
       </span>
       You haven't added anything to your cart yet!
-    </h3>
-  )}
-</div>
+          </h3>
+        )}
+    </div>
   );
 };
 
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
@@ -10,6 +11,7 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
+    idbPromise('cart', 'delete', { ...item });
   };
 
   const onChange = (e) => {
@@ -20,44 +22,47 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id
       });
+
+      idbPromise('cart', 'delete', { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
-    }
-  };
 
-  return (
-    <div className="flex-row">
-      <div>
-        <img
-          src={`/images/${item.image}`}
-          alt=""
-        />
-      </div>
-      <div>
-        <div>{item.name}, ${item.price}</div>
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+    }
+
+    return (
+      <div className="flex-row">
         <div>
-          <span>Qty:</span>
-          <input
-            type="number"
-            placeholder="1"
-            value={item.purchaseQuantity}
-            onChange={onChange}
+          <img
+            src={`/images/${item.image}`}
+            alt=""
           />
-          <span
-            role="img"
-            aria-label="trash"
-            onClick={() => removeFromCart(item)}
-          >
-            🗑️
+        </div>
+        <div>
+          <div>{item.name}, ${item.price}</div>
+          <div>
+            <span>Qty:</span>
+            <input
+              type="number"
+              placeholder="1"
+              value={item.purchaseQuantity}
+              onChange={onChange}
+            />
+            <span
+              role="img"
+              aria-label="trash"
+              onClick={() => removeFromCart(item)}
+            >
+              🗑️
 </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default CartItem;
+  export default CartItem;
